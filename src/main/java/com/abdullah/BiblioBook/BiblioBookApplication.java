4177df2de183960e231f3c436312ceb15d1cfb59
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-06 14:22:11 +0300

    init commit.

---
+++ src/main/java/com/abdullah/BiblioBook/BiblioBookApplication.java
@@ -0,0 +1,13 @@
+package com.abdullah.BiblioBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdullah/BiblioBook/BiblioBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.BiblioBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
